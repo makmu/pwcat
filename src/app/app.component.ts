4c0a8a46a12772521812067e5281e9feef26a568
Thomas Heid <Thomas.Heid@astrum-it.de>
2018-08-27 16:31:50 +0200

    local storage

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,21 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'my-app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
+  ngOnInit(): void {
+    var tmpCounter = window.localStorage.getItem("counter")
+    console.log(tmpCounter)
+    if (tmpCounter !== null){
+      this.counter = parseInt(tmpCounter);
+    }
+  }
   counter = 0;
   increaseCounter() {
     this.counter += 1;
+    localStorage.setItem("counter", this.counter.toString())
   }
 }
